37e3098109efa4ddb8b2d69d74fa3f98f4e3a71b
Spencer Christensen <spencer.christensen88888@gmail.com>
2018-06-05 15:51:54 -0700

    fix admin site to allow approval of question submissions and add question submission form to navbar

--- questions/admin.py
+++ questions/admin.py
@@ -1,3 +1,23 @@
 from django.contrib import admin
+from .models import Question
 
-# Register your models here.
+
+def make_approved(modeladmin, request, queryset):
+    return queryset.update(approved=True)
+
+
+def make_unapproved(modeladmin, request, queryset):
+    return queryset.update(approved=False)
+
+
+make_approved.short_description = "Approve selected questions"
+make_unapproved.short_description = "Unapprove selected questions"
+
+
+@admin.register(Question)
+class QuestionAdmin(admin.ModelAdmin):
+    date_hierarchy = "created"
+    list_display = ("body", "category", "difficulty",)
+    list_filter = ("approved",)
+    ordering = ("-created",)
+    actions = (make_approved, make_unapproved,)
