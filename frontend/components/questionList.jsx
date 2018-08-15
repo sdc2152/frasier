a504f746539080486594a5e989a54f66080a3db4
Spencer Christensen <spencer.christensen88888@gmail.com>
2018-08-15 10:48:05 -0700

    update questionList

--- frontend/components/questionList.jsx
+++ frontend/components/questionList.jsx
@@ -2,26 +2,32 @@ import React from "react";
 import {connect} from "react-redux";
 import {fetchQuestionList} from "../actions/questionListActions";
 
-const QuestionList = ({questionList, fetchQuestionList}) => {
-  const {list} = questionList;
-
-  // TODO: change this so i don't have to do the list length check
-  // look up lifecycle method to use maybe or do initial check
-  if (list.length === 0) {
-    console.log("here");
-    fetchQuestionList();
+class QuestionList extends React.Component {
+  constructor(props) {
+    super(props);
   }
 
-  const questions = list.map((c, i) =>(<li key={i}>{c.body}</li>));
-  return (
-    <div className="container">
-      <h1>Questions List</h1>
-      <ul>
-        {questions}
-      </ul>
-    </div>
-  );
-};
+  componentWillMount() {
+    this.props.fetchQuestionList();
+  }
+
+  render() {
+    const {list} = this.props.questionList;
+    const questions = list.map((c, i) =>(
+      <li key={i}>
+        {c.body}
+      </li>
+      ));
+    return (
+      <div className="container">
+        <h1>Questions List</h1>
+        <ul>
+          {questions}
+        </ul>
+      </div>
+    );
+  }
+}
 
 const mapStateToProps = state => (
   {
