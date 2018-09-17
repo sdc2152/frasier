e56ef14a6b3529d222199919947f4a6c569c5b5d
Spencer Christensen <spencer.christensen88888@gmail.com>
2018-09-17 13:09:50 -0700

    change allowed host

--- frasier/settings.py
+++ frasier/settings.py
@@ -17,7 +17,7 @@ SECRET_KEY = os.environ.get(
 DEBUG = bool(os.environ.get("DJANGO_DEBUG", True))
 
 ALLOWED_HOSTS = [
-    "enigmatic-escarpment-88378.herokuapp.com/",
+    "enigmatic-escarpment-88378.herokuapp.com",
 ]
 
 
